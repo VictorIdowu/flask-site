a6abc9785ad6bdf091eeb27afd92ed5bf2a2da91
VictorIdowu <idsonvictor@gmail.com>
2024-06-09 00:33:54 +0100

    flask basics

--- app.py
+++ app.py
@@ -1,27 +1,69 @@
-from flask import Flask,redirect,url_for
+from flask import Flask,redirect,url_for,request,render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file =  "sqlite:///{}".format(os.path.join(project_dir,"mydatabase.db"))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+  name = db.Column(db.String(100),unique=True,nullable=False,primary_key=True)
+  author = db.Column(db.String(100),nullable=False)
 
 @app.route('/')
-def home():
-  return '<h1>This is the Home Page<h1> <a href="/profile/John">John</a>'
+def index():
+  return render_template('index.html')
 
-@app.route('/admin')
-def welcome_admin():
-  return '<h1>Welcome Admin<h1> <a href="/">Home</a>'
+@app.route('/addbook')
+def addbook():
+  return render_template('addbook.html')
 
-@app.route('/guest/<guest>')
-def welcome_guest(guest):
-  return '<h1>Welcome %s</h1> <a href="/">Home</a>' % guest
+@app.route('/submitbook', methods=['POST'])
+def submitbook():
+  name = request.form['name']
+  author = request.form['author']
+  book = Book(name=name,author=author)
+  db.session.add(book)
+  db.session.commit()
+  return redirect('/books')
 
-@app.route('/user/<name>')
-def welcome_user(name):
-  if name == 'admin':
-    return redirect(url_for('welcome_admin'))
-  else:
-    return redirect(url_for('welcome_guest',guest=name))
+@app.route('/profile/<username>')
+def profile(username):
+  return render_template('profile.html', username=username, isActive=False)
+
+@app.route('/books')
+def books():
+  books = Book.query.all() 
+  return render_template('books.html', books=books)
+
+@app.route('/updatebooks')
+def updatebooks():
+  books = Book.query.all()
+  return render_template('updatebooks.html',books=books)
+
+@app.route('/update', methods=['POST'])
+def update():
+  newname = request.form['newname']
+  oldname = request.form['oldname']
+  newauthor = request.form['newauthor']
+
+  book = Book.query.filter_by(name=oldname).first()
+  book.name = newname
+  book.author = newauthor
+  db.session.commit()
+  return redirect('/books')
+
+@app.route('/delete',methods=['POST'])
+def delete():
+  name = request.form['name']
+  book = Book.query.filter_by(name=name).first()
+  db.session.delete(book)
+  db.session.commit()
+  return redirect('/books')
 
 
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True)
