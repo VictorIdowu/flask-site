274da7a3d29d2f41bed9523bac7a3d072f3fc663
VictorIdowu <idsonvictor@gmail.com>
2024-06-08 12:12:08 +0100

    ...

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'This is the Home Page- <a href="/new">New</a>'
+  return '<h1>This is the Home Page<h1> <a href="/profile/John">John</a>'
 
-@app.route('/new')
-def new():
-  return 'This is a new page <a href="/">Home</a>'
+@app.route('/admin')
+def welcome_admin():
+  return '<h1>Welcome Admin<h1> <a href="/">Home</a>'
+
+@app.route('/guest/<guest>')
+def welcome_guest(guest):
+  return '<h1>Welcome %s</h1> <a href="/">Home</a>' % guest
+
+@app.route('/user/<name>')
+def welcome_user(name):
+  if name == 'admin':
+    return redirect(url_for('welcome_admin'))
+  else:
+    return redirect(url_for('welcome_guest',guest=name))
 
 
-app.run()
+
+
+app.run(debug=True)
